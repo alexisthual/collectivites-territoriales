a97374ad501075e71459d4590125c915b7d9fb9e
alexis <alexisthual@gmail.com>
2020-01-25 00:02:15 +0100

    Initial commit 🚀

---
+++ pages/index.tsx
@@ -0,0 +1,46 @@
+import { Layout, Menu, Icon } from 'antd';
+
+const { Content, Footer, Sider } = Layout;
+
+export default () => (
+  <Layout className="page-layout">
+    <Sider
+      breakpoint="lg"
+      collapsedWidth="0"
+      onBreakpoint={broken => {
+        console.log(broken);
+      }}
+      onCollapse={(collapsed, type) => {
+        console.log(collapsed, type);
+      }}
+    >
+      <div className="logo" />
+      <Menu theme="dark" mode="inline" defaultSelectedKeys={['4']}>
+        <Menu.Item key="1">
+          <Icon type="user" />
+          <span className="nav-text">nav 1</span>
+        </Menu.Item>
+        <Menu.Item key="2">
+          <Icon type="video-camera" />
+          <span className="nav-text">nav 2</span>
+        </Menu.Item>
+        <Menu.Item key="3">
+          <Icon type="upload" />
+          <span className="nav-text">nav 3</span>
+        </Menu.Item>
+        <Menu.Item key="4">
+          <Icon type="user" />
+          <span className="nav-text">nav 4</span>
+        </Menu.Item>
+      </Menu>
+    </Sider>
+    <Layout>
+      <Content>
+        <div>content</div>
+      </Content>
+      <Footer className="footer">
+        Coolectivités Territoriales ©2020
+      </Footer>
+    </Layout>
+  </Layout>
+)
