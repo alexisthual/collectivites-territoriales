008f04086b4aea358f3597a3459625c4be60ff27
alexis <alexisthual@gmail.com>
2020-01-25 04:05:43 +0100

    Add [year]/[type] endpoint and handle state changes in app

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,7 @@
+import fetch from 'isomorphic-unfetch'
 import { Col, Icon, Input, Layout, List, Row, Select, Tabs, Typography } from 'antd';
+import { useRouter } from 'next/router'
+import { useState, useEffect } from 'react'
 
 import SimpleTab from '../components/SimpleTab'
 
@@ -8,111 +11,162 @@ const { Option } = Select;
 const { TabPane } = Tabs;
 const { Title } = Typography;
 
-const onChange = (event) => {
-  console.log(event)
-};
-
-const handleChange = (value) => {
-  console.log(value)
-}
-
 const data = [
   {name: "Ile de France"},
   {name: "Polynésie française"}
 ]
 
-export default () => (
-  <Layout className="page-layout">
-    <Sider
-      breakpoint="md"
-      collapsedWidth="0"
-      onBreakpoint={broken => {
-        console.log(broken);
-      }}
-      onCollapse={(collapsed, type) => {
-        console.log(collapsed, type);
-      }}
-      width={250}
-    >
-      <Row gutter={[20, 12]}>
-        <Col span={22} offset={1}>
-          <Title className="app-title">Coolectivités</Title>
-        </Col>
-      </Row>
-      <Row gutter={[0, 12]}>
-        <Col span={22} offset={1}>
-          <Select className="full-width" defaultValue="2019">
-            <Option value="2015">2015</Option>
-            <Option value="2016">2016</Option>
-            <Option value="2017">2017</Option>
-            <Option value="2018">2018</Option>
-            <Option value="2019">2019</Option>
-          </Select>
-        </Col>
-      </Row>
-      <Row gutter={[0, 12]}>
-        <Col span={22} offset={1}>
-          <Select className="full-width" defaultValue="d">
-            <Option value="r" disabled>Régions</Option>
-            <Option value="d">Départements</Option>
-            <Option value="c" disabled>Communes</Option>
-          </Select>
-        </Col>
-      </Row>
-      <Row gutter={[0, 12]}>
-        <Col span={22} offset={1}>
-          <Search
-            className="full-width"
-            placeholder="Nom de la collectivité"
-          />
-        </Col>
-      </Row>
-      <Row gutter={[0, 8]}>
-        <Col span={24}>
-          <List
-            dataSource={data}
-            renderItem={item => (
-              <List.Item key={item.name}>
-                <List.Item.Meta
-                  title={<a href="https://ant.design">{item.name}</a>}
-                />
-              </List.Item>
-            )}
-          >
-          </List>
-        </Col>
-      </Row>
-    </Sider>
-    <Layout>
-      <Content style={{margin: "5px"}}>
-        <div className="card-container">
-          <Tabs defaultActiveKey="simple" type="card">
-            <TabPane
-              key="simple"
-              tab={
-                <span>
-                  <Icon type="bulb" />
-                  Vue simple
-                </span>
-              }
+const MainPage = ({ y=2018, t="d", q=null }) => {
+  const router = useRouter()
+  const [year, setYear] = useState(y)
+  const [type, setType] = useState(t)
+  const [query, setQuery] = useState(q)
+
+  const handleState = () => {
+    console.log("here")
+    console.log([year, type])
+    if (year && type) {
+      router.push('/[year]/[type]', `/${year}/${type}`)
+    }
+  }
+
+  const yearChanged = (value) => {
+    console.log(`year changed ${value}`)
+    setYear(value)
+  }
+
+  const typeChanged = (value) => {
+    console.log(`type changed ${type}`)
+    setType(value)
+  }
+
+  const queryChanged = (value) => {
+    setQuery(value)
+  }
+
+  useEffect(() => {
+    if (year && type) {
+      router.push('/[year]/[type]', `/${year}/${type}`)
+    }
+  }, [year, type])
+
+  return (
+    <Layout className="page-layout">
+      <Sider
+        breakpoint="md"
+        collapsedWidth="0"
+        onBreakpoint={broken => {
+          console.log(broken);
+        }}
+        onCollapse={(collapsed, type) => {
+          console.log(collapsed, type);
+        }}
+        width={250}
+      >
+        <Row gutter={[20, 12]}>
+          <Col span={22} offset={1}>
+            <Title className="app-title">Coolectivités</Title>
+          </Col>
+        </Row>
+        <Row gutter={[0, 12]}>
+          <Col span={22} offset={1}>
+            <Select
+              className="full-width"
+              defaultValue={year}
+              onChange={yearChanged}
             >
-              <SimpleTab />
-            </TabPane>
-            <TabPane
-              disabled
-              key="advanced"
-              tab={
-                <span>
-                  <Icon type="thunderbolt" />
-                  Vue avancée
-                </span>
-              }
+              <Option value="2015">2015</Option>
+              <Option value="2016">2016</Option>
+              <Option value="2017">2017</Option>
+              <Option value="2018">2018</Option>
+              <Option value="2019">2019</Option>
+            </Select>
+          </Col>
+        </Row>
+        <Row gutter={[0, 12]}>
+          <Col span={22} offset={1}>
+            <Select
+              className="full-width"
+              defaultValue={type}
+              onChange={typeChanged}
             >
-              Tab 2
-            </TabPane>
-          </Tabs>
-        </div>
-      </Content>
+              <Option value="r" disabled>Régions</Option>
+              <Option value="d">Départements</Option>
+              <Option value="c" disabled>Communes</Option>
+            </Select>
+          </Col>
+        </Row>
+        <Row gutter={[0, 12]}>
+          <Col span={22} offset={1}>
+            <Search
+              className="full-width"
+              placeholder="Nom de la collectivité"
+              value={query}
+              onChange={queryChanged}
+            />
+          </Col>
+        </Row>
+        <Row gutter={[0, 8]}>
+          <Col span={24}>
+            <List
+              dataSource={data}
+              renderItem={item => (
+                <List.Item key={item.name}>
+                  <List.Item.Meta
+                    title={<a href="https://ant.design">{item.name}</a>}
+                  />
+                </List.Item>
+              )}
+            >
+            </List>
+          </Col>
+        </Row>
+      </Sider>
+      <Layout>
+        <Content style={{margin: "5px"}}>
+          <div className="card-container">
+            <Tabs defaultActiveKey="simple" type="card">
+              <TabPane
+                key="simple"
+                tab={
+                  <span>
+                    <Icon type="bulb" />
+                    Vue simple
+                  </span>
+                }
+              >
+                <SimpleTab />
+              </TabPane>
+              <TabPane
+                disabled
+                key="advanced"
+                tab={
+                  <span>
+                    <Icon type="thunderbolt" />
+                    Vue avancée
+                  </span>
+                }
+              >
+                Tab 2
+              </TabPane>
+            </Tabs>
+          </div>
+        </Content>
+      </Layout>
     </Layout>
-  </Layout>
-)
+  )
+}
+
+// MainPage.getInitialProps = async ({ req, query }) => {
+//   const protocol = req
+//     ? `${req.headers['x-forwarded-proto']}:`
+//     : location.protocol
+//   const host = req ? req.headers['x-forwarded-host'] : location.host
+//   const pageRequest = `${protocol}//${host}/api/profiles?page=${query.page ||
+//     1}&limit=${query.limit || 9}`
+//   const res = await fetch(pageRequest)
+//   const json = await res.json()
+//   return json
+// }
+
+export default MainPage
