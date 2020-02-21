937bda4b06732cb6239590fcaa5b0c789c49e480
alexis <alexisthual@gmail.com>
2020-02-21 16:41:27 +0100

    Directly return entries

--- api/entries/index.js
+++ api/entries/index.js
@@ -6,11 +6,12 @@ const escape = require('sql-template-strings')
 
 module.exports = async (req, res) => {
   const year = parseInt(req.query.year) || 2018
-  console.log(`YEAR ${year}`)
+
+  console.log(`Selecting departments...`)
 
   const entries = await db.query(`SELECT ndept FROM data WHERE exer=${year} GROUP BY ndept;`)
-  console.log(`ENTRIES`)
-  console.log(entries)
 
-  res.status(200).json({ entries })
+  console.log(`Selected ${entries.length} departments`)
+
+  res.status(200).json(entries)
 }
