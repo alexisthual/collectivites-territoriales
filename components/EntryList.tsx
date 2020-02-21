326ccaeab1c5e55c25c2363a6b9e23280e22468b
alexis <alexisthual@gmail.com>
2020-02-21 16:42:07 +0100

    Implement callback for EntryList

--- components/EntryList.tsx
+++ components/EntryList.tsx
@@ -1,7 +1,13 @@
-const EntryList = ({ entries }) => (
+const EntryList = ({ entries, callback }) => (
   <div className="entry-list">
     {entries ? entries.map((item: any, index: number) => (
-      <div key={index} className="entry-item">{item.ndept}</div>
+      <div
+        key={index}
+        className="entry-item"
+        onClick={() => callback(item.ndept)}
+      >
+        {item.ndept}
+      </div>
     )) : null}
   </div>
 )
