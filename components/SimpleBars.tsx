17e31b4aa62e64896c938a2298af708c075374f3
alexis <alexisthual@gmail.com>
2020-02-24 23:31:02 +0100

    Remove mock data from bar component

--- components/SimpleBars.tsx
+++ components/SimpleBars.tsx
@@ -1,54 +1,6 @@
 import { Bar } from '@nivo/bar'
 import { useState, useEffect } from 'react'
 
-// const bar_mock_data = [
-//   {
-//     "country": "AD",
-//     "hot dog": 196,
-//     "hot dogColor": "hsl(67, 70%, 50%)",
-//     "burger": 9,
-//     "burgerColor": "hsl(133, 70%, 50%)",
-//     "sandwich": 98,
-//     "sandwichColor": "hsl(64, 70%, 50%)",
-//     "kebab": 3,
-//     "kebabColor": "hsl(254, 70%, 50%)",
-//     "fries": 74,
-//     "friesColor": "hsl(239, 70%, 50%)",
-//     "donut": 95,
-//     "donutColor": "hsl(280, 70%, 50%)"
-//   },
-//   {
-//     "country": "AE",
-//     "hot dog": 156,
-//     "hot dogColor": "hsl(119, 70%, 50%)",
-//     "burger": 72,
-//     "burgerColor": "hsl(163, 70%, 50%)",
-//     "sandwich": 31,
-//     "sandwichColor": "hsl(14, 70%, 50%)",
-//     "kebab": 148,
-//     "kebabColor": "hsl(111, 70%, 50%)",
-//     "fries": 29,
-//     "friesColor": "hsl(288, 70%, 50%)",
-//     "donut": 71,
-//     "donutColor": "hsl(274, 70%, 50%)"
-//   },
-//   {
-//     "country": "AF",
-//     "hot dog": 195,
-//     "hot dogColor": "hsl(63, 70%, 50%)",
-//     "burger": 159,
-//     "burgerColor": "hsl(183, 70%, 50%)",
-//     "sandwich": 135,
-//     "sandwichColor": "hsl(200, 70%, 50%)",
-//     "kebab": 48,
-//     "kebabColor": "hsl(284, 70%, 50%)",
-//     "fries": 156,
-//     "friesColor": "hsl(124, 70%, 50%)",
-//     "donut": 88,
-//     "donutColor": "hsl(4, 70%, 50%)"
-//   },
-// ]
-
 interface DataPoint {
   COMPTE_T: number,
   value: number,
@@ -77,9 +29,6 @@ const SimpleBars = ({ data, height, width }: {data: DataPoint[], height: number,
           k.push(d.year.toString())
         }
       })
-
-      console.log(k)
-      console.log(Object.values(a))
 
       setKeys(k.sort())
       setBarsData(Object.values(a))
